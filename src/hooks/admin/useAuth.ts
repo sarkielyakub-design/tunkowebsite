a72c9b591ccadda65f2f146682d35d8e11f31983
your name <you@email.com>
2026-08-28 15:05:55 +0100

    Fix admin authentication

--- src/hooks/admin/useAuth.ts
+++ src/hooks/admin/useAuth.ts
@@ -1,19 +1,34 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import * as AuthService from "@/src/services/admin/auth";
 
+/**
+ * Admin Login
+ */
 export const useLogin = () =>
   useMutation({
     mutationFn: AuthService.login,
   });
 
+/**
+ * Admin Logout
+ */
 export const useLogout = () =>
   useMutation({
     mutationFn: AuthService.logout,
   });
 
-export const useProfile = () =>
-  useQuery({
-    queryKey: ["admin-profile"],
-    queryFn: AuthService.me,
-    retry: false,
+/**
+ * Forgot Admin Password
+ */
+export const useForgotPassword = () =>
+  useMutation({
+    mutationFn: AuthService.forgotPassword,
+  });
+
+/**
+ * Reset Admin Password
+ */
+export const useResetPassword = () =>
+  useMutation({
+    mutationFn: AuthService.resetPassword,
   });
